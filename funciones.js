382a1438018211688fec7156de0733b9ce3b75da
carotavano <145924840+carotavano@users.noreply.github.com>
2023-09-24 14:24:09 -0300

    Create funciones.js

---
+++ funciones.js
@@ -0,0 +1,5 @@
+document.getElementById("botton"). onclick = function () {
+    console.log("caputramos el evento clic");
+    document.getElementById("demo").innerHTML = "https://linkedin.com/AdrianaNesse"
+
+}
